719212c325559d47ae8512028b17418ccbf80772
Kiruna13 <thomas.ducoulombier@free.fr>
2022-02-28 22:35:23 +0100

    Design de la page login/register

---
+++ app/src/main/java/fontsmaterialuiux/cairoTextView.java
@@ -0,0 +1,41 @@
+package fontsmaterialuiux;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+/**
+ * for more visit http://materialuiux.com
+ */
+public class cairoTextView extends AppCompatTextView
+{
+
+    public cairoTextView(Context context)
+    {
+        super(context);
+        init();
+    }
+
+    public cairoTextView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    public cairoTextView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init()
+    {
+        if (!isInEditMode())
+        {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Cairo-Regular.ttf");
+            setTypeface(tf);
+        }
+    }
+}
